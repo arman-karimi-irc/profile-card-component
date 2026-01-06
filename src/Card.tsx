437763cd0a4bbd5b23d0d0c880e03f8d58c1696c
iirccopilot <iirccopilot@gmail.com>
2026-01-06 11:57:25 +0330

    feat(card): avatar + top bg

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,5 +1,23 @@
 export default function Card() {
-    return <div>
-        
+  return (
+    <div className="w-full h-full px-2 flex items-center justify-center">
+      <div className="bg-white w-72 h-[40vh] rounded-xl shadow-md overflow-hidden relative">
+        {/* Top background pattern */}
+        <img
+          src="/images/bg-pattern-card.svg"
+          alt="card background"
+          className="w-full"
+        />
+
+        {/* Avatar */}
+        <div className="flex justify-center">
+          <img
+            src="/images/image-victor.jpg"
+            alt="avatar"
+            className="w-24 h-24 rounded-full border-4 border-white -mt-12"
+          />
+        </div>
+      </div>
     </div>
+  );
 }
